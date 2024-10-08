853797275202e349f789d34d0ae276cc5b0d871b
bartkrekelberg <bart@vision.rutgers.edu>
2024-10-08 08:55:45 +0200

    Bugfixes

--- +lm/plotPerSubject.m
+++ +lm/plotPerSubject.m
@@ -1,48 +1,70 @@
-function [effects,ci] = plotPerSubject(m,pv)
-% For a given linear model based on one or more subjects, refit the model
-% per subject and show the results to get an idea of variability in the
-% sample.
+function [effectsTable,ciTable,pTable,models] = plotPerSubject(m,pv)
+% For a given linear model based on one or more groups (e.g., subjects), refit the model
+% per level of the group (subject) and show the results for a subset of model coefficients
+% to get an idea of  variability across the different levels.
 %
 % INPUT
 % glm -  a (generalized) linear mixed model
 %
 % OUTPUT
-% effects  - table with fixed effects. First row is the group, the other
-%               rows are the individual subjects
+% effects  - table with fixed effects. First column is the group, the other
+%               columns are the individual subjects
 % ci - table with confidence intervals.
+% p - table with p-values
 %
 % BK - Feb 2020.
 arguments
     m (1,1)  % Linear model
+    pv.coefficients (1,:) string = ""        % Show only these coefficients  (Defaults to all model coefficients)
+    pv.zscore (1,:) string = ""              % Z-score these variables in the model for each level of the grouping variable. Defaults to none.
     pv.showHistogram (1,1) logical = false  % Histogram of effects across subjects
     pv.showLine (1,1)logical = true         % Line graph of effects for each subject
-    pv.NUMPRECISION (1,1) double {mustBeInteger,mustBeNonnegative} =3; % num2str for effects
+    pv.showGroup (1,1) logical = true       % Add the group level result as a red line
+    pv.NUMPRECISION (1,1) double {mustBeInteger,mustBeNonnegative} =3; % num2str for coefficients
+    pv.alpha (1,1) double = 0.05;
 end
 
-
+assert(all(pv.coefficients=="") || all(ismember(pv.coefficients,m.CoefficientNames)),"pv.coefficients should list the names of coefficients in the model");
+assert(all(pv.zscore =="") || all(ismember(pv.zscore,m.CoefficientNames)),"pv.zscore should list the names of coefficients in the model");
+%% Extract the data from the full model,
+T = m.Variables(~m.ObservationInfo.Excluded,m.VariableInfo.InModel | ismember(m.VariableNames,m.Formula.ResponseName));
 dummyVarCoding = lm.dummyVarCoding(m);
-
-%% Createa a table of fixed effects and confidence intervals
-fe = m.fixedEffects;
-fe(1) =[]; % Remove intercept
-feNames = m.CoefficientNames;
-feNames(1) = [];
-effects = table(fe,'RowNames',feNames,'VariableNames',{'Group'});
-ci = [m.Coefficients.Lower m.Coefficients.Upper];
-ci(1,:) = [];
-cis  = table(ci,'RowNames',feNames,'VariableNames',{'Group'});
-
-%% Now fit each subject separately and add to the tables
-%
-T = m.Variables(:,m.VariableInfo.InModel | ismember(m.VariableNames,m.Formula.ResponseName));
-
-subjects = unique(T{:,m.Formula.GroupingVariableNames{:}}); % only keep subjects who're relevant to this condition
-
+% Determine which variable represents the grouping (e.g. subject)
+assert(isscalar(m.Formula.GroupingVariableNames) && (isscalar(m.Formula.GroupingVariableNames{1}{1}) || ischar(m.Formula.GroupingVariableNames{1}{1})),'ploePerSubject currently only works for a single grouping variable (e.g. (1|subject))')
+groupName = m.Formula.GroupingVariableNames{1}{1};
+groupLevels = unique(T.(groupName));
 formula = m.Formula.char;
-for s=subjects'
+
+%% Createa the output tables and add the results for the full sample
+feNames = m.CoefficientNames;
+if pv.coefficients == ""
+    pv.coefficients = string(feNames(2:end));
+end
+keepCoeffs = ismember(feNames,pv.coefficients);
+fe = m.fixedEffects;
+fe = fe(keepCoeffs);
+feNames =feNames(keepCoeffs);
+effectsTable = table(fe,'RowNames',feNames,'VariableNames',{'Group'});
+ci = [m.Coefficients.Lower m.Coefficients.Upper];
+ciTable  = table(ci(keepCoeffs,:),'RowNames',feNames,'VariableNames',{'Group'});
+pTable  = table(m.Coefficients.pValue(keepCoeffs,:),'RowNames',feNames,'VariableNames',{'Group'});
+
+%% Now fit each group level separately and add to the tables
+nrGroupLevels = numel(groupLevels);
+if nargout>3
+    models= cell(1,nrGroupLevels);
+end
+sCntr =0;
+for s=groupLevels'
+    sCntr= sCntr+1;
     try
         % Extract the relevant subset of data for this subjects
-        thisT = T(~m.ObservationInfo.Excluded & T.subject==s,:);
+        thisT = T(T.(groupName)==s,:);
+        if pv.zscore ~=""
+            for factor = pv.zscore
+                thisT.(factor) = zscore(thisT.(factor));
+            end
+        end
         % Refit for this subject
         if isa(m,'LinearMixedModel')
             thisGlm = fitlme(thisT,formula,'FitMethod',m.FitMethod,'DummyVarCoding',dummyVarCoding) ;
@@ -56,69 +78,81 @@ for s=subjects'
                 error(msg);
             end
         end
-        
+        if nargout>3
+            models{sCntr} = thisGlm; %#ok<AGROW>
+        end
+        % Store in the output tables
         fe = thisGlm.fixedEffects;
-        fe(1) =[]; % Remove intercept
+        fe = fe(keepCoeffs);
         thisFeNames = thisGlm.CoefficientNames;
-        thisFeNames(1) = [];
-
-        effects = [effects   table(fe,'VariableNames',string(s))]; %#ok<AGROW>
+        thisFeNames = thisFeNames(keepCoeffs);
+        effectsTable = [effectsTable   table(fe,'VariableNames',string(s))]; %#ok<AGROW>
         ci = [thisGlm.Coefficients.Lower thisGlm.Coefficients.Upper];
-        ci(1,:) = [];
-        cis  = [cis table(ci,'VariableNames',string(s))];%#ok<AGROW>
-
+        ciTable  = [ciTable table(ci(keepCoeffs,:),'VariableNames',string(s))];%#ok<AGROW>
+        pTable = [pTable table(thisGlm.Coefficients.pValue(keepCoeffs),'VariableNames',string(s))]; %#ok<AGROW>
         %Sanity check that the order of FE is the same in the per subject and
-        %group model. This can fail if one of the subjects does not
+        %group model. This can fail if one of the group levels (subjects) does not
         % have a complete set of conditions
         assert(all(strcmpi(feNames,thisFeNames)),'coefficientNames of individual fit NOT matched with group fit! (check ''dummyVarCoding'' settings)');
     catch me
         fprintf('perSubject lmm for %s failed on %s (%s)\n',formula,s,me.message)
         continue
     end
-
 end
 
 %% Visualize the main group and individual results
-nrSubjects = width(effects)-1;
-nrEffects = height(effects);
-nrRows = sum(pv.showHistogram+pv.showLine);
+if pv.showHistogram || pv.showLine
+    clf;
+    nrGroups = width(effectsTable)-1;
+    nrEffects = height(effectsTable);
+    nrRows = sum(pv.showHistogram+pv.showLine);
+    nrCols = nrEffects;
+    for e =1:nrEffects
+        if pTable{e,1} <pv.alpha
+            sigStr = '(*)';
+        else
+            sigStr = '';
+        end
+        titleStr = [feNames{e} ': ' num2str(effectsTable{e,1},pv.NUMPRECISION) ' ' sigStr] ;
+        titleStr = char(titleStr,sprintf('Same sign: %d/%d ',sum(sign(effectsTable{e,1})==sign(effectsTable{e,2:end})),nrGroups));
+        titleStr = char(titleStr,sprintf('Significant (alpha = %.3f): %d/%d',pv.alpha, sum(pTable{e,2:end}<pv.alpha),nrGroups));
 
-for e =1:nrEffects
-    if pv.showHistogram
-        subplot(nrRows,nrEffects,e);
-        % Top row shows histogram of effects
-        histogram(effects{e,2:end});
+        if pv.showHistogram
+            h1= subplot(nrRows,nrCols,e);
+            % Top row shows histogram of effects
+            histogram(effectsTable{e,2:end});
+            title(titleStr,'Interpreter','None');
+            xlabel([feNames{e} ' Effect'],'Interpreter','none')
+        end
 
+        if pv.showLine
+            h2= subplot(nrRows,nrCols,e+nrCols*pv.showHistogram);
+            % Bottom row shows line plots with CI per group level (subject)
+            line(reshape(ciTable{e,2:end},[2 nrGroups]),repmat(1:nrGroups,[2 1]),'Color','k')
+            hold on
+            plot(effectsTable{e,2:end},1:nrGroups,'k.')
+            line([0 0],[0 nrGroups])
+            if pv.showGroup
+            % Show the group effect as a red line in the middle
+            y = nrGroups/2 + mod(nrGroups+1,2)/2;
+            plot(effectsTable{e,1},y,'r*')
+            line(ciTable{e,1},y*[1 1],'Color','r','LineWidth',2);
+            end
+            ylim([0 nrGroups]);
+            set(gca,'yTickLabels',{})
+            if e==1
+                ylabel(groupName + "#")
+                set(gca,'yTick',1:nrGroups,'ytickLabel',groupLevels)
+            end
+            if ~pv.showHistogram
+                title(titleStr,'Interpreter','None');
+            end
+            xlabel([feNames{e} ' Effect'],'Interpreter','none')
+        end
 
-    end
-
-    if pv.showLine
-
-        subplot(nrRows,nrEffects,e+nrEffects*pv.showHistogram);
-        % Bottom row shows line plots with CI per subject.
-        line(reshape(cis{e,2:end},[2 nrSubjects]),repmat(1:nrSubjects,[2 1]),'Color','k')
-        hold on
-        plot(effects{e,2:end},1:nrSubjects,'k.')
-        line([0 0],[0 nrSubjects])
-        % Show the group effect as a red line
-        line(cis{e,1},0.5+nrSubjects/2*[1 1],'Color','r','LineWidth',2);
-        ylim([0 nrSubjects]);
-        set(gca,'yTickLabels',{})
-        if e==1
-            ylabel 'Subject #'
-            set(gca,'yTick',1:nrSubjects,'ytickLabel',subjects)
+        if pv.showHistogram && pv.showLine
+            linkaxes([h1 h2],'x')
         end
     end
-
-    if prod(sign(cis{e,1})) >0 % CI both <0 or both >0; significant at alpha level.
-        sigStr = '(*)';
-    else
-        sigStr = '';
-    end
-    t=  title([feNames{e} ': ' num2str(effects{e,1},pv.NUMPRECISION) ' ' sigStr]);
-    t.Interpreter = 'None';
-
-    xlabel 'Effect'
-
 end
 
--- +lm/randomSlope.m
+++ +lm/randomSlope.m
@@ -9,16 +9,22 @@ function [lmeWithRandomSlope,stats,reTable] = randomSlope(lme,pv)
 % will then return the LMM for 'output ~ input + (1|subject) + (-1 + input|subject)'
 % and return stats for the comparison of this model with the original
 % (outpuf of linearmixedmodel/compare).
+%
 % To also generate a table of random effects and evaluate whether
 % individual subjects had significant overall effects of input, specify
-% the level of the factor at which you want to evaluate this. 
-% 
-% 
+% the contrast that you want to evaluate.
+% For instance
+% randomSlope(lme,pv.factor= "input", pv.group = "subject",contrast =
+% {"A","B"})
+% Will determine the contrast between input ="A" and input ="B" that
+% includes random + fixed effects, for each subject (And assess
+% significance for each subject).
+%
 % Note
 % The reTable always has the output from linearmixedmodel/randomEffects, which
 % assesses whether a random effect is different from zero (pValue, tStat,
 % DF). If a level is provided in the call to this function, this table also
-% contains columns that determine whether the (total= fixed+random) slope effect 
+% contains columns that determine whether the (total= fixed+random) slope effect
 %  at the given level was significantly different from zero for each level of the group
 % (p,F,df1,df2) (Output of coefTes, using a 'REContrast' input).
 %
@@ -27,20 +33,28 @@ arguments (Input)
     lme (1,1)                       % The original LMM
     pv.factor (1,1) string          % The factor in the LMM for which to add a slope random effect
     pv.group (1,1) string           % Grouping variable for the random effect
-    pv.level (1,1) = NaN            % Level of the factor at which to evaluate significance for each level of the grouping variable
-    
+    pv.contrast (1,:) cell ={}      % Categorical level of factor at which to evaluate the contrast and  significance for each level of the grouping variable
+    pv.exclude (:,1) logical =[] 
+
     pv.plot (1,1) logical = false   % Show a histogram of effects
     pv.alpha (1,1) double = 0.05    % Alpha level (plot only)
+    pv.tail (1,1) string = "both"
 end
 arguments (Output)
     lmeWithRandomSlope (1,1)        % The model with the added slope
-    stats table                     % Comparison of the extended model with the original (output of compare)    
-    reTable table                   % Table with estimates of the random effects for the group                                                       
+    stats table                     % Comparison of the extended model with the original (output of compare)
+    reTable table                   % Table with estimates of the random effects for the group
 end
 
 % Construct the new formula and compute the model
 newFormula = sprintf([char(lme.Formula) ' + (-1 + %s|%s)'], pv.factor , pv.group);
-lmeWithRandomSlope= fitlme(lme.Variables,newFormula,'Exclude',lme.ObservationInfo.Excluded,'DummyVarCoding',lm.dummyVarCoding(lme));
+dvCoding = lm.dummyVarCoding(lme);
+
+exclude = lme.ObservationInfo.Excluded;
+if ~isempty(pv.exclude)
+    exclude = exclude | pv.exclude;
+end
+lmeWithRandomSlope= fitlme(lme.Variables,newFormula,'Exclude',exclude,'DummyVarCoding',dvCoding);
 % Compare the new with the old model
 if nargout>1 || pv.plot
     stats = dataset2table(compare(lme,lmeWithRandomSlope));
@@ -49,57 +63,105 @@ end
 if nargout >2 || pv.plot
     [~,~,reTable] = randomEffects(lmeWithRandomSlope);
     reTable =convertvars(dataset2table(reTable),@iscellstr,"string");
+    isSlopeRe = reTable.Group==pv.group & (reTable.Name == pv.factor | startsWith(reTable.Name,pv.factor +"_"));
+    grpLevels = reTable.Level(isSlopeRe);
+    nrGrpLevels = numel(grpLevels);
 end
 
-% Evaluate significance of the factor at the specified level
-if  (isstring(pv.level) && pv.level~="") || (isnumeric(pv.level) && ~isnan(pv.level))
-    % Find the fixed effect
-    if lme.VariableInfo{pv.factor,"IsCategorical"}
-        level = pv.factor+ "_" + pv.level;
-        isFe  = lmeWithRandomSlope.Coefficients.Name == level;
-        fe = lmeWithRandomSlope.Coefficients.Estimate(isFe);
-        feContrast = double(isFe);
+
+if  ~isempty(pv.contrast)
+    % Determine the fixed effect contrast.
+    feContrast = lm.contrast(lmeWithRandomSlope,{pv.factor,pv.contrast{1}},{pv.factor,pv.contrast{2}});
+
+    % Evaluate significance of the factor at the specified level
+    thisVarInfo = lmeWithRandomSlope.VariableInfo(strcmp(pv.factor,lmeWithRandomSlope.VariableInfo.Row),:);
+
+    % the reTable will have entries for all levels, except the
+    % first(reference coded) or last (effects coded).
+    if strcmpi(dvCoding,"REFERENCE")
+        inTable = thisVarInfo.Range{1}(2:end);
     else
-        isFe  = lmeWithRandomSlope.Coefficients.Name == pv.factor;
-        fe = lmeWithRandomSlope.Coefficients.Estimate(isFe)*pv.level;
-        feContrast = double(isFe)*pv.level;
+        inTable = thisVarInfo.Range{1}(1:end-1);
     end
-    levels = reTable.Level(reTable.Group==pv.group);
-    nrLevels = numel(levels);
-    tIndividual = table('size',[nrLevels 5],'VariableNames',{'Level','p','F','df1','df2'},'VariableTypes',{'string', 'double','double','double','double'});
-    for lvl =1:nrLevels
-        reContrast = double(reTable.Level == levels(lvl));
-        tIndividual.Level(lvl) = levels(lvl);
-        [tIndividual.p(lvl),tIndividual.F(lvl), tIndividual.df1(lvl),tIndividual.df2(lvl)] = coefTest(lmeWithRandomSlope,feContrast',0,'REContrast',reContrast');
+
+
+
+   
+    tIndividual = table('size',[nrGrpLevels 6],'VariableNames',{'Contrast','Level','p','F','df1','df2'},'VariableTypes',{'double','string', 'double','double','double','double'});
+    for grpLvl =1:nrGrpLevels
+        stayThisGrp = reTable.Level == grpLevels(grpLvl);
+        % Build the RE contrast
+        if ~ismember(pv.contrast{1},inTable)
+            % Level 1 is not in the model - construct from others
+            if dvCoding=="reference"
+                lvl1 = zeros(height(reTable),1);
+            else
+                lvl1 = -stayThisGrp;
+            end
+        else
+            % level 1 is in the model
+            lvl1 = double(stayThisGrp & ismember(reTable.Name,pv.factor +"_" + pv.contrast{1}));
+        end
+
+        if ~ismember(pv.contrast{2},inTable)
+            % Level 2 is not in the model - construct from others
+            if dvCoding=="reference"
+                lvl2 = zeros(height(reTable),1);
+            else
+                lvl2 = -stayThisGrp;
+            end
+        else
+            % level 2 is in the model
+            lvl2 = double(stayThisGrp & ismember(reTable.Name,pv.factor +"_" + pv.contrast{2}));
+        end
+        reContrast = (lvl1-lvl2);
+        tIndividual.Level(grpLvl) = grpLevels(grpLvl);
+        [tIndividual.p(grpLvl),tIndividual.F(grpLvl), tIndividual.df1(grpLvl),tIndividual.df2(grpLvl)] = coefTest(lmeWithRandomSlope,feContrast,0,'REContrast',reContrast');
+        tIndividual.Contrast(grpLvl) = reContrast'*reTable.Estimate +feContrast*lmeWithRandomSlope.Coefficients.Estimate;
     end
-    reTable = reTable(reTable.Group==pv.group,:);  % Keep only the specifeid group
+    name = pv.factor + ":" + pv.contrast{1} + "-" + pv.contrast{2};
+    tIndividual= addvars(tIndividual,repmat(name,height(tIndividual),1),'NewVariableNames','ContrastName');
     reTable = innerjoin(reTable,tIndividual);
-    individualSignificance = true;
-    estimate= reTable.Estimate +fe;
-else
-    reTable = reTable(reTable.Group==pv.group,:);
-    estimate= reTable.Estimate; % Just show the random effects below
-    individualSignificance = false;
+
+    isSlopeRe = reTable.Group==pv.group & (reTable.Name == pv.factor | startsWith(reTable.Name,pv.factor +"_")); 
+    reTable = reTable(isSlopeRe,:); % Return only the slope effects that were added.
+   
+    estimate = reTable.Contrast;  % Show the estimated contrasts in the plot below
+    p = reTable.p;
+    xLbl  = name + " Fixed +";
+else    
+
+    isSlopeRe = reTable.Group==pv.group & (reTable.Name == pv.factor | startsWith(reTable.Name,pv.factor +"_")); 
+    reTable = reTable(isSlopeRe,:); % Return only the slope effects that were added.
+    estimate = reTable.Estimate ;  % Show the random effects in the plot below
+    p = reTable.pValue;
+    xLbl  = pv.factor;
 end
 
-if pv.plot   
-    [allC,edges]= histcounts(estimate); 
+ switch lower(pv.tail)
+     case "both"
+         isSignificant = p <pv.alpha;
+     case "left"
+         isSignificant  =estimate<0 & p < pv.alpha;         
+     case "right"
+         isSignificant = estimate>0 & p < pv.alpha;
+ end
+
+if pv.plot
+    [allC,edges]= histcounts(estimate);
     centers = edges(1:end-1) + (edges(2:end)-edges(1:end-1))/2;
     bar(centers',allC','b');
-    xlabel(pv.factor + " Random Effect")      
+    xlabel(xLbl + " Random Effect",'Interpreter','none')
     str= sprintf('AIC %d (Chi^2 (%d) = %3.3f, p= %3.3g)',round(diff(stats.AIC)),stats.deltaDF(2),stats.LRStat(2),stats.pValue(2));
-    if individualSignificance    
-        isSignificant = (reTable.p<pv.alpha);
-        sigC = histcounts(estimate(isSignificant),edges); 
-        hold on
-        h= bar(centers',sigC','r');
-        h.FaceAlpha=0.8;        
-        xlabel(pv.factor + " Fixed+Random Effect")
-        legend('All',sprintf('p<%.3f',pv.alpha));
-        str = [str sprintf('\n %d/%d (%.2f%%) significant',sum(isSignificant),nrLevels,100*mean(isSignificant))];
-    end
+    sigC = histcounts(estimate(isSignificant),edges);
+    hold on
+    h= bar(centers',sigC','r');
+    h.FaceAlpha=0.8;
+
+    legend('All',sprintf('p<%.3f',pv.alpha));
+    str = [str sprintf('\n %d/%d (%.2f%%) significant',sum(isSignificant),nrGrpLevels,100*mean(isSignificant))];
     ylabel("#" + pv.group)
-    title (str)
+    title (str,'Interpreter','none')
     hold off
 end
 
