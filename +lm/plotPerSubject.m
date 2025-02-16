f8d991c1c5685cfab6d640c8c2f999e11be3d32a
bartkrekelberg <bart@vision.rutgers.edu>
2025-02-16 12:32:25 +0100

    Rewrote plotPerSubject to allow specification of effects to compute./

--- +lm/plotPerSubject.m
+++ +lm/plotPerSubject.m
@@ -1,4 +1,4 @@
-function [effects,ci] = plotPerSubject(m,pv)
+function [effectsT,ciT] = plotPerSubject(m,pv)
 % For a given linear model based on one or more subjects, refit the model
 % per subject and show the results to get an idea of variability in the
 % sample.
@@ -17,27 +17,28 @@ arguments
     pv.showHistogram (1,1) logical = false  % Histogram of effects across subjects
     pv.showLine (1,1)logical = true         % Line graph of effects for each subject
     pv.NUMPRECISION (1,1) double {mustBeInteger,mustBeNonnegative} =3; % num2str for effects
+    pv.effects (1,:) string = ""            % Which effects to show. Defaults to all except intercept.Set to "*" to include the intercept.
 end
-
 
 dummyVarCoding = lm.dummyVarCoding(m);
 
 %% Createa a table of fixed effects and confidence intervals
 fe = m.fixedEffects;
-fe(1) =[]; % Remove intercept
 feNames = m.CoefficientNames;
-feNames(1) = [];
-effects = table(fe,'RowNames',feNames,'VariableNames',{'Group'});
+effectsT = table(fe,'RowNames',feNames,'VariableNames',{'Group'});
 ci = [m.Coefficients.Lower m.Coefficients.Upper];
-ci(1,:) = [];
-cis  = table(ci,'RowNames',feNames,'VariableNames',{'Group'});
+ciT  = table(ci,'RowNames',feNames,'VariableNames',{'Group'});
 
+
+if pv.effects ==""
+    pv.effects = feNames(2:end); % Exclude intercept
+elseif pv.effects == "*"
+    pv.effects = feNames; % Include intercept
+end
 %% Now fit each subject separately and add to the tables
-%
+
 T = m.Variables(:,m.VariableInfo.InModel | ismember(m.VariableNames,m.Formula.ResponseName));
-
 subjects = unique(T{:,m.Formula.GroupingVariableNames{:}}); % only keep subjects who're relevant to this condition
-
 formula = m.Formula.char;
 for s=subjects'
     try
@@ -57,51 +58,46 @@ for s=subjects'
             end
         end
         
-        fe = thisGlm.fixedEffects;
-        fe(1) =[]; % Remove intercept
+        fe = thisGlm.fixedEffects;       
         thisFeNames = thisGlm.CoefficientNames;
-        thisFeNames(1) = [];
-
-        effects = [effects   table(fe,'VariableNames',string(s))]; %#ok<AGROW>
+        % Some fe in the per-subject fit may be ordered differently; make
+        % sure they are placed in the correct row of the effects table.
+        [tf,order] =ismember(thisFeNames,feNames);
+        assert(all(tf),'Missing FE in the per-subject fit for %s.',s);
+        effectsT = [effectsT   table(fe(order),'VariableNames',string(s))]; %#ok<AGROW>               
         ci = [thisGlm.Coefficients.Lower thisGlm.Coefficients.Upper];
-        ci(1,:) = [];
-        cis  = [cis table(ci,'VariableNames',string(s))];%#ok<AGROW>
-
-        %Sanity check that the order of FE is the same in the per subject and
-        %group model. This can fail if one of the subjects does not
-        % have a complete set of conditions
-        assert(all(strcmpi(feNames,thisFeNames)),'coefficientNames of individual fit NOT matched with group fit! (check ''dummyVarCoding'' settings)');
+        ciT  = [ciT table(ci(order,:),'VariableNames',string(s))];%#ok<AGROW>        
     catch me
         fprintf('perSubject lmm for %s failed on %s (%s)\n',formula,s,me.message)
         continue
     end
-
 end
 
 %% Visualize the main group and individual results
-nrSubjects = width(effects)-1;
-nrEffects = height(effects);
-nrRows = sum(pv.showHistogram+pv.showLine);
+nrSubjects = width(effectsT)-1;
+% Restrict effects 
+keep = ismember(effectsT.Properties.RowNames,pv.effects);
+effectsT  =effectsT(keep,:);
+ciT = ciT(keep,:);
 
+nrEffects = height(effectsT);
+nrRows = sum(pv.showHistogram+pv.showLine);
 for e =1:nrEffects
     if pv.showHistogram
         subplot(nrRows,nrEffects,e);
         % Top row shows histogram of effects
-        histogram(effects{e,2:end});
-
-
+        histogram(effectsT{e,2:end});
     end
 
     if pv.showLine
-
         subplot(nrRows,nrEffects,e+nrEffects*pv.showHistogram);
         % Bottom row shows line plots with CI per subject.
-        line(reshape(cis{e,2:end},[2 nrSubjects]),repmat(1:nrSubjects,[2 1]),'Color','k')
+        line(reshape(ciT{e,2:end},[2 nrSubjects]),repmat(1:nrSubjects,[2 1]),'Color','k')
         hold on
-        plot(effects{e,2:end},1:nrSubjects,'k.')
+        plot(effectsT{e,2:end},1:nrSubjects,'k.')
         line([0 0],[0 nrSubjects])
         % Show the group effect as a red line
-        line(cis{e,1},0.5+nrSubjects/2*[1 1],'Color','r','LineWidth',2);
+        line(ciT{e,1},0.5+nrSubjects/2*[1 1],'Color','r','LineWidth',2);
         ylim([0 nrSubjects]);
         set(gca,'yTickLabels',{})
         if e==1
@@ -110,15 +106,14 @@ for e =1:nrEffects
         end
     end
 
-    if prod(sign(cis{e,1})) >0 % CI both <0 or both >0; significant at alpha level.
+    if prod(sign(ciT{e,1})) >0 % CI both <0 or both >0; significant at alpha level.
         sigStr = '(*)';
     else
         sigStr = '';
     end
-    t=  title([feNames{e} ': ' num2str(effects{e,1},pv.NUMPRECISION) ' ' sigStr]);
+    t=  title([effectsT.Properties.RowNames{e} ': ' num2str(effectsT{e,1},pv.NUMPRECISION) ' ' sigStr]);
     t.Interpreter = 'None';
-
     xlabel 'Effect'
-
+    hold off
 end
 
